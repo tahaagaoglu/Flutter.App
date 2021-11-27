71b63f09fa7b7f26d2c57275a0b48d59cedb6ae5
tahaga61 <tahaga61@gmail.com>
2021-11-28 00:50:07 +0300

    tahagaoglu

---
+++ lib/screens/bmi_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:globo_fitness/shared/menu_bottom.dart';
+import 'package:globo_fitness/shared/menu_drawer.dart';
+
+class BmiScreen extends StatelessWidget {
+  const BmiScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text('BMI Calculator')),
+        bottomNavigationBar: MenuBottom(),
+        drawer: MenuDrawer(),
+        body: Container(
+            child: Container(
+          child: Text('sa'),
+        )));
+  }
+}
